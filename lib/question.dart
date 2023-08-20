b4b0b5bc4b1fc80f794cf73d72db0c15e705c1b5
dewMyth <akalankadewmith@gmail.com>
2023-08-20 10:57:53 +0530

    add base app

---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question {
+  String questionText = "";
+  bool questionAnswer = false;
+
+  Question({required String question, required bool answer}) {
+    questionText = question;
+    questionAnswer = answer;
+  }
+}
